a0aebae1700951bb19710323ee24dcf636fa6ba7
Pr3d1ct3d <orthecharchar@gmail.com>
2026-06-08 21:38:56 +0300

    Clean up index.js by removing commented code
    Removed commented-out code and retained the functional counting logic.

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-/*
-window.alert('bombo clat');
-document.getElementById("myH1").textContent = "hello";
-document.getElementById("myP").textContent = "i like piza";
-
-let fullname = "Or hamer";
-let age = 15;
-let isStudent = true;
-
-document.getElementById("P1").textContent = `Your name is ${fullname}`;
-document.getElementById("P2").textContent = `You are ${age} years old`;
-document.getElementById("P3").textContent = `Enrolled: ${isStudent}`;
-
-let username
-
-document.getElementById("mySumb").onclick = function(){
-  username = document.getElementById("myText").value;
-  document.getElementById("h1").textContent = `Hello, ${username}`
-}
-
-
-let age = window.prompt("how old is u");
-age=Number(age)
-age+=1;
-
-console.log(age);
-*/
-
 const decreasBtn = document.getElementById("decreasBtn");
 const resetBtn = document.getElementById("resetBtn");
 const increaseBtn = document.getElementById("increaseBtn");
